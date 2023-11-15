c261f6150172b19c91e3625d633a6ef75c6cfb7f
RicardoAymay <r.aymay.goncalves@gmail.com>
2023-11-15 11:36:19 -0300

    feat: button added. How to use the button added

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import arrow from "../../assets/imgs/arrow.png"
+import Image from 'next/image'
+interface iButtomProps{
+    buttomType: "submit" | "button" | "reset"
+    style: "brown" | "white" | "arrow"
+    buttonText: string
+}
+
+
+
+const buttonStyles = (style: string) =>{
+    if (style === "brown"){
+        return "w-40 h-12 tracking-widest font-bold flex items-center text-primary-white justify-center bg-primary-brown hover:bg-primary-skin"
+    }
+    if (style === "white"){
+        return "w-40 h-12 tracking-widest border border-black font-bold flex items-center text-primary-black justify-center bg-primary-white hover:bg-primary-black hover:text-white"
+    }
+    if (style === "arrow"){
+      return "w-40 h-12 flex items-center text-primary-black justify-center bg-primary-white hover:bg-primary-black"
+  }
+}
+const Button = ({buttomType, style, buttonText}: iButtomProps) => {
+  return (
+    <button type={buttomType} className={buttonStyles(style)}>
+        {buttonText} {style === "arrow" ? <Image src={arrow} className='ml-5' alt='Go see this!'/> : ''}
+    </button>
+  )
+}
+
+export default Button
